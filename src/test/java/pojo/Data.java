b6b720f0a4c2e2d5df9cfdda131aa1ce35b3e655
Dung Office <mr.dungdoan2506@gmail.com>
2021-10-28 15:42:47 +0700

    data class added

---
+++ src/test/java/pojo/Data.java
@@ -0,0 +1,23 @@
+package pojo;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import pageObject.Register_Page;
+import resources.Base;
+
+public class Data extends Base {
+	public static void addCustomer(Register_Page r,String country) {
+		r.fullName().sendKeys("Test1");
+		r.preferred().sendKeys("Test2");
+		r.email().sendKeys("test1@gmail.com");
+		r.selectCountry().click();
+		List<WebElement> countryListElement = driver.findElements(By.xpath("//div[@class='q-item__label']"));
+		//List<String> countryL =.collect(Collectors.toList())
+				//countryListElement.stream().map(m->m.getText()).filter(f->f.contains("Viet")).
+	}
+}
