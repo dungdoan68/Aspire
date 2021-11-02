d0e298b95e1e6c20a115ddd1e3cbbaa104e97dcc
Dung <adung.doan@gmail.com>
2021-11-03 00:23:10 +0700

    nothingChanged

--- src/test/java/pageObject/Register_Page.java
+++ src/test/java/pageObject/Register_Page.java
@@ -1,7 +1,9 @@
 package pageObject;
 
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import resources.Base;
 
@@ -11,14 +13,66 @@ public class Register_Page extends Base{
 		this.driver = driver;
 	}
 	
-	By fullName = By.xpath("");
-	By preferred = By.xpath("");
-	By email = By.xpath("");
-	By mobileNumber = By.xpath("");
-	By getInfoForm = By.xpath("");
-	By promoCode = By.xpath("");
-	By agreeCheckbox = By.xpath("");
-	By btnContinue = By.xpath("");
-	By loginPage = By.xpath("");
-	By iframe = By.xpath("");
+
+	By fullName = By.xpath("//input[@name='full_name']");
+	By preferred =  By.xpath("//input[@name='preferred_name']");
+	By email = By.xpath("//input[@name='email']");
+	By selectCountry = By.xpath("//img[@class='q-img__image q-img__image--with-transition q-img__image--loaded']");
+	By mobileNumber = By.xpath("//input[@name='phone']");
+	By getInfoForm = By.xpath("//input[@class='q-field__input q-placeholder col']");
+	By promoCode = By.xpath("//input[@data-cy='register-person-referral-code']");
+	By chkAgree = By.xpath("//div[@class='q-checkbox__bg absolute']");
+	By applyCode = By.xpath("//span[contains(text(),'Apply')]");
+	By btnContinue = By.xpath("//span[contains(text(),'Con')]");
+	By loginPage = By.xpath("//span[contains(text(),'Login')]");
+	By skip = By.xpath("//span[contains(text(),'Skip')]");
+
+	By iframe = By.xpath("//fc_widget");
+	
+	public WebElement fullName() {
+		return driver.findElement(fullName);
+	}
+	public WebElement skip() {
+		return driver.findElement(skip);
+	}
+	public WebElement preferred() {
+		return driver.findElement(preferred);
+	}
+	public WebElement applyCode() {
+		return driver.findElement(applyCode);
+	}
+	public WebElement email() {
+		return driver.findElement(email);
+	}
+	public WebElement selectCountry() {
+		return driver.findElement(selectCountry);
+	}
+	public WebElement mobileNumber() {
+		return driver.findElement(mobileNumber);
+	}
+	public WebElement getInfoForm() {
+		return driver.findElement(getInfoForm);
+	}
+	public WebElement promoCode() {
+		return driver.findElement(promoCode);
+	}
+	public WebElement chkAgree() {
+		return driver.findElement(chkAgree);
+	}
+	public VerifyOTP_Page btnContinue() {
+		driver.findElement(btnContinue).click();
+		VerifyOTP_Page v = new VerifyOTP_Page(driver);
+		return v;
+	}
+	public Login_page loginPage() {
+		driver.findElement(loginPage).click();
+		Login_page l = new Login_page(driver);
+		return l;
+				
+	}
+	public Chatbox_page iframe() {
+		driver.switchTo().frame(driver.findElement(iframe));
+		Chatbox_page c = new Chatbox_page(driver);
+		return c;
+	}
 }
---
+++ src/test/java/stroies/customer/cells/CreateCustomer.story
@@ -0,0 +1,8 @@
+Sample story
+
+					 
+Scenario: Customer does not have an account
+Given step represents a precondition to an event
+When usre input all info to form
+Then they will tranfer to verify OTP mobile phone page 
+					 
